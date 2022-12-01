10f0c7adaee3a1a3def84df9ad28ca450e33e80c
Bryan Herbst <bryan.herbst@target.com>
2022-12-01 09:01:37 -0600

    Day 1 ✅

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,32 @@
 fun main() {
+
+    fun getCalories(input: List<String>): List<Int> {
+        var index = 0;
+        return input.fold(mutableListOf(0)) { values, line ->
+            if (line.isBlank()) {
+                index++
+                values.add(0)
+            } else {
+                values[index] += line.toInt()
+            }
+            values
+        }
+    }
+
     fun part1(input: List<String>): Int {
-        return input.size
+        return getCalories(input).max()
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        return getCalories(input)
+            .sortedDescending()
+            .take(3)
+            .sum()
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 24000)
 
     val input = readInput("Day01")
     println(part1(input))
